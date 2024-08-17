0797e2b1f0a5e92ff57127684d0093898f122c23
nirchoudhary <139118342+nirchoudhary@users.noreply.github.com>
2024-08-17 17:08:40 +0000

    dataType summery

--- .devcontainer/1_basic/3_conversionOperation.js
+++ .devcontainer/1_basic/3_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = "33"
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+//console.log(typeof valueInNumber);
 
 // "33" = 33
 //"33abc" = NaN
@@ -13,7 +13,7 @@ console.log(typeof valueInNumber);
 let isLogggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLogggedIn)
-console.log(booleanIsLoggedIn );
+//console.log(booleanIsLoggedIn );
 
 //1 = true ; 0 = false
 // "" = false 
@@ -22,6 +22,30 @@ console.log(booleanIsLoggedIn );
 let someNumber = 100
 
 let stringconvert = String(someNumber)
-console.log(stringconvert);
-console.log(typeof stringconvert);
+//console.log(stringconvert);
+//console.log(typeof stringconvert);
+
+//********************** operations ************** */
+
+let value = 3
+let negValue = -value
+
+//console.log(negValue);
+
+// console.log("1" + 2); //12
+// console.log(1+ "2"); //12
+// console.log("1"+ 2 + 2 );//122
+// console.log(1 + 2 + "2"); //32
+
+let gameCounter = 100;
+++gameCounter
+console.log(gameCounter);
+//linke to studey 
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
+
+
+
+
+
+
 
---
+++ .devcontainer/1_basic/5_dataTypes_summery.js
@@ -0,0 +1,34 @@
+// Primitive (it is call by value)
+
+//7 catagories: String ,Number, Booleam, Null,Undefined, Symbol, BigInt
+
+// ****javascript is Dynamic lsnguage****
+
+// const id = Symbol('123');
+// const AnotherId = Symbol('123');
+
+// console.log(typeof(AnotherId));
+// console.log(typeof(id));
+// console.log(id === AnotherId);
+
+
+
+
+
+// reference Type(Non- premitive)
+
+//Arrays , Objects, Functions 
+
+// const heros =["shaktiman"," Krish" , "Nagrjun" , "Bahubali"]
+
+let myobj={
+    name: "Nir",
+    age: 22,
+}
+
+// const myFunction = function(){
+//     console.log(myobj);
+// }
+console.log(myobj);
+
+
