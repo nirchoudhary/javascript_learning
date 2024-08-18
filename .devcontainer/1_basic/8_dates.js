b09b8be6ec2985a45eae70c2a8b6d6af79aa8616
nirchoudhary <139118342+nirchoudhary@users.noreply.github.com>
2024-08-18 15:00:57 +0530

    dates

---
+++ .devcontainer/1_basic/8_dates.js
@@ -0,0 +1,16 @@
+// let myDate = new Date() //Date is an object
+// console.log(myDate);//2024-08-18T09:03:00.626Z
+// console.log(myDate.toString());//Sun Aug 18 2024 14:35:03 GMT+0530 (India Standard Time)
+// console.log(myDate.toDateString());//Sun Aug 18 2024
+// console.log(myDate.toISOString());//2024-08-18T09:07:15.941Z
+// console.log(myDate.toJSON());//2024-08-18T09:09:13.823Z
+// console.log(myDate.toLocaleString());//      18/8/2024, 2:39:13 pm
+// console.log(myDate.toLocaleDateString());//  18/8/2024
+// console.log(myDate.toLocaleTimeString());//  2:39:13 pm
+
+
+let myCreatedDate = new Date()
+console.log(myCreatedDate.toLocaleString());
+
+let newDate = new Date()
+console.log(newDate.getMonth()+1);
