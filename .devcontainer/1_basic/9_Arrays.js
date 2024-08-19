1ded29736bfe791755f01683d5e79a16c0721ee7
nirchoudhary <139118342+nirchoudhary@users.noreply.github.com>
2024-08-19 14:41:39 +0530

    Arrays

---
+++ .devcontainer/1_basic/9_Arrays.js
@@ -0,0 +1,23 @@
+// Array 
+
+const myArr = [0,1,2,3,3,];
+const myHeros = ["Mom ","Dad"]
+
+const MyArr2 = new Array(7,8,9,4,6,)
+console.log(MyArr2[1]);
+
+// *******************************************
+
+//Array methods 
+
+MyArr2.push(10) //[ 7, 8, 9, 4, 6, 10 ]
+MyArr2.pop()
+MyArr2.shift();
+MyArr2.unshift()
+
+console.log(MyArr2);
+
+console.log(MyArr2.includes(10))
+console.log(MyArr2.indexOf(9));
+
+
