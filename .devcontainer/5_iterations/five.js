721713c94be52d549b7860b5791d000a57602783
nirchoudhary <139118342+nirchoudhary@users.noreply.github.com>
2024-08-27 11:15:04 +0530

    foreach complete

---
+++ .devcontainer/5_iterations/five.js
@@ -0,0 +1,18 @@
+// const coding = ["js", "ruby","java","python","cpp"]
+
+// [if the forEach loop the value are not returnable ] 
+
+// const value = coding.forEach((items) =>{
+//     console.log(items);
+//     return items
+// })
+// console.log(value);  
+
+//[and the filter we can Easy to  return the values]
+
+const myNumber = [1,2,3,4,5,6,7,8,9,10]
+
+const newNumber = myNumber.filter((num) => num> 5)
+
+   console.log(newNumber);
+   
